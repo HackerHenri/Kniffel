bb8738fd93c031dbd807ddb48c914d871b390f23
Luca Mainitz <luca@mainitz.de>
2023-12-22 22:54:06 +0100

    Refactor initiate_game.php to handle player validation and redirect

--- initiate_game.php
+++ initiate_game.php
@@ -7,11 +7,11 @@
     $password = "";
 
     if (!$player1 || !$player2) {
-        echo "Bitte geben Sie zwei Spielernamen ein!";
+        header("Location: index.php");
         exit;
     }
     else if ($player1 == $player2) {
-        echo "Bitte geben Sie zwei unterschiedliche Spielernamen ein!";
+        header("Location: index.php");
         exit;
     }
     else {
@@ -22,10 +22,22 @@
         $sql = 'TRUNCATE TABLE score';
         mysqli_query($conn, $sql);
 
-    // Insert player1 and player2 into score table
-    $sql = "INSERT INTO `score`(`playername`) VALUES ('$player1'),('$player2');";
-    mysqli_query($conn, $sql);
+        // Insert player1 and player2 into score table
+        $sql = "INSERT INTO `score`(`playername`) VALUES ('$player1'),('$player2');";
+        mysqli_query($conn, $sql);
 
-    $conn->close();
+        // Read the SQL server response
+        $response = mysqli_affected_rows($conn);
+
+        if ($response == 2) {
+            header("Location: game.php");
+            exit;
+        }
+        else {
+            header("Location: index.php");
+            exit;
+        }
+
+        $conn->close();
     }
 ?>
