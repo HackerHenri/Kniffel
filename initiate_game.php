868c6d6b13288615e24dfe73410343032ca08108
Luca Mainitz <luca@mainitz.de>
2023-12-28 13:06:43 +0100

    sql tabellen clearing angepasst

--- initiate_game.php
+++ initiate_game.php
@@ -29,6 +29,16 @@
         // Read the SQL server response
         $response = mysqli_affected_rows($conn);
 
+        // delete all entries from tasks
+        $sql = 'TRUNCATE TABLE activeround';
+        mysqli_query($conn, $sql);
+
+        // Insert standard values into activeround table
+        $sql = "INSERT INTO `activeround`(`dicelocked1`, `dicelocked2`, `dicelocked3`, `dicelocked4`, `dicelocked5`, `rollCounter`, `diceScore1`, `diceScore2`, `diceScore3`, `diceScore4`, `diceScore5`) VALUES ('0','0','0','0','0','0','1','2','3','4','5');";
+        mysqli_query($conn, $sql);
+
+        $conn->close();
+
         if ($response == 2) {
             header("Location: game.php");
             exit;
@@ -37,7 +47,5 @@
             header("Location: index.php");
             exit;
         }
-
-        $conn->close();
     }
 ?>
