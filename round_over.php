b7986eaa8f9a8bbeb115b60aecdc09e754cbd716
Luca Mainitz <luca@mainitz.de>
2023-12-29 11:46:23 +0100

    Add game result display and play again button

--- round_over.php
+++ round_over.php
@@ -8,14 +8,48 @@
     <title>Henris Würfelspiel</title> 
 
     <link id="style1" rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
 </head>
 
 <body class="container">
   <h1 id="h1" class="item">Das Spiel ist vorbei!</h1>
-
+  
   <div class="container">
-    <label class="item">Spieler XYZ hat gewonnen.</label>
-    <button class="button">Erneut Spielen</button>
+    <?php
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+
+        // ##### Create connection #####
+        $conn = mysqli_connect($servername, $username, $password, "kniffel");
+
+        $sql = "SELECT * FROM `score` ORDER BY `score`.`id` DESC LIMIT 1";
+        $result1 = mysqli_query($conn, $sql);
+        $row1 = mysqli_fetch_assoc($result1);
+        $player1 = $row1['playername'];
+        $player1score = $row1['gesamt'];
+
+        $sql = "SELECT * FROM `score` ORDER BY `score`.`id` DESC LIMIT 1,1";
+        $result2 = mysqli_query($conn, $sql);
+        $row2 = mysqli_fetch_assoc($result2);
+        $player2 = $row2['playername'];
+        $player2score = $row2['gesamt'];
+
+        if ($player1score > $player2score) {
+            echo "<label class='item'>Spieler $player1 hat gewonnen!</label>";
+        } else if ($player1score < $player2score) {
+            echo "<label class='item'>Spieler $player2 hat gewonnen!</label>";
+        } else if ($player1score == $player2score){
+            echo "<label class='item'>Unentschieden! Beide Spieler haben $player1Score Punkte erreicht</label>";
+        }
+        else {
+            echo "<label class='item'>Fehler!</label>";
+        }
+
+    ?>
+    <br>
+    <button class="button" onclick="exitGame()">Erneut Spielen</button>
     </div>
 </body>
 
