f0e059eff118548691301fce75e61a971546d71c
HackerHenri <supahenri@gmail.com>
2024-01-02 14:04:24 +0100

    fixed api call to initiate_game.php, delete .idea

--- initiate_game.php
+++ initiate_game.php
@@ -1,6 +1,22 @@
 <?php
-    $player1 = $_GET['spieler1'];
-    $player2 = $_GET['spieler2'];
+
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    $json = file_get_contents('php://input');
+    // echo $json
+    $data = json_decode($json, true);
+    echo $data['player1'];
+    var_dump($data);
+
+    if (isset($data)) {
+    echo json_encode(['status' => 'success', 'message' => 'Message received: ' . $data['player1']]);
+    } else {
+    echo json_encode(['status' => 'error', 'message' => 'No message received']);
+    }
+
+    $player1 = $data['player1'];
+    $player2 = $data['player2'];
 
     $servername = "localhost";
     $username = "root";
